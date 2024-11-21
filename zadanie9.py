eac7c5481c6eb14d40c0ed4be4f3493e360ee338
Kiwanek <poczta@kevinkarlinski.com>
2024-11-21 11:54:28 +0100

    Update zadanie9.py

---
+++ zadanie9.py
@@ -0,0 +1,4 @@
+for i in range(1, 101):
+    if i % 7 == 0:  # Sprawdzamy, czy liczba jest podzielna przez 7
+        print(f"Pierwsza liczba podzielna przez 7 to: {i}")
+        break  # Zatrzymujemy pętlę po znalezieniu liczby
